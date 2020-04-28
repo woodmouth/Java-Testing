0d89651e7a41daf2c8307711c90f3dfcc2838d82
linweiwen <linweiwen@qiyi.com>
2020-04-28 10:49:46 +0800

    Repo inited

---
+++ src/com/edwin/LinkedListTest.java
@@ -0,0 +1,18 @@
+package com.edwin;
+
+import java.util.*;
+
+/**
+ * This program demostrates operations on linked list
+ * @version 1.0 2020-04-28
+ * @author Edwin Lin
+ */
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+	// write your code here
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        System.out.println("Testing");
+    }
+}
